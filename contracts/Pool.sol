69da02b00bd122ae46ef1009fadbc45b04472477
Harshavardha Jagannathan <harsha31.jagan@utexas.edu>
2021-10-31 02:40:36 -0500

    lottery code

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -33,7 +33,7 @@ interface FakeFarm {
 
 contract Pool {
     uint256 totalAmt;
-    address counterAddr;
+    // address counterAddr;
     address WBNBAddr;
     address cakeAddr;
     PancakeRouter swapper;
@@ -45,6 +45,10 @@ contract Pool {
 
     mapping(address => uint256) public stakingAmts;
     uint256 numPpl;
+    uint256 randNonce;
+    address[] names;
+
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         // Underscore is a special character only used inside
@@ -66,7 +70,7 @@ contract Pool {
         WBNBAddr = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
         cakeAddr = 0xF9f93cF501BFaDB6494589Cb4b4C15dE49E85D0e;
         numPpl = 0;
-        baseYield = 5;
+        baseYield = 5; //percent
     }
 
     function setSwapper(address _contract) public {
@@ -89,22 +93,55 @@ contract Pool {
         // if(stakingAmts[msg.sender] == null){
 
         // } else{
-
+        if(stakingAmts[msg.sender] == 0){
+            names[numPpl] = msg.sender;
+            numPpl++;
+        }
         stakingAmts[msg.sender] += amts[0];
-        // }
+        
         totalAmt += amts[0];
         // cakeContract.transfer(recipient, amount);
+    }
+
+    function random(uint256 max) private returns (uint256) {
+        randNonce++; 
+        return uint256(keccak256(abi.encodePacked(now,
+                                                msg.sender,
+                                                randNonce))) % max;
+        // return uint8(uint256(keccak256(abi.encodePacked(_text, _num, _addr))%max);
     }
 
     function lottery() public onlyOwner {
         fakeFarm.stake();
 
         uint256 newTotal = cakeContract.balanceOf(address(this));
-        // for(uint256 i = 0; i<stakingAmts.)
+        uint32 factor = 1000;
+        uint256[] memory slots = new uint256[](numPpl);
+        uint256 totSlots = 0;
+        //[1, 5, 4] 
+        for(uint256 i = 0; i<numPpl; i++){
+            uint256 contribution = stakingAmts[names[i]];
+            uint256 numSlots = (contribution / totalAmt) * factor;
+            slots[i] = numSlots;
+            totSlots += numSlots;
+        }
+
+        uint256 numWinners = (numPpl > 100) ? 10 : 1;
+        uint256 winAmt = (newTotal - totalAmt) / numWinners;
+        for(uint8 i = 0; i<numWinners; i++){
+            uint256 winner = random(totSlots + 1);
+            //0.1 * 
+            for(uint256 j=0; j < numPpl; j++){
+                if(slots[j] < winner) winner -= slots[j];
+                //have found j as winner
+                stakingAmts[names[j]] += winAmt;
+            }
+        }
     }
 
     function withdraw() public {
         // do lottery, pick wallets of participants proportional to deposit
+        
     }
 
     function test() public pure returns (string memory) {
